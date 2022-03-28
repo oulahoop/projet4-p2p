80b5cf631e8317e6ce2de894aa090a898996aa27
oulahoop <maelcaubere@live.fr>
2022-03-28 14:23:01 +0200

    ok

--- chat.go
+++ chat.go
@@ -5,18 +5,22 @@ import (
 	"fmt"
 	"io"
 	"log"
+	"math/rand"
 	"os"
+	"reflect"
 	"strconv"
 	"strings"
 	"sync"
+	"time"
 )
 
 // Tout ce code est ajouté pour le mini-projet 3
 
 type chatMessage struct {
-	from    string
-	content string
-	private bool
+	from      string
+	content   string
+	fromClock map[string]int
+	private   bool
 }
 
 type peerCollection struct {
@@ -27,10 +31,8 @@ type peerCollection struct {
 // Lit les messages sur la connexion du pair p et recopie
 // ces messages dans le canal receiveChan (ce qui permettra
 // ensuite de les afficher)
-func (p peer) receiveMessages(receiveChan chan chatMessage) {
-
+func (p peer) receiveMessages(receiveChan chan chatMessage, myClock *clock) {
 	reader := bufio.NewReader(p.conn)
-
 	for {
 		msg, err := reader.ReadString('\n')
 		if err != nil {
@@ -40,15 +42,28 @@ func (p peer) receiveMessages(receiveChan chan chatMessage) {
 			}
 			log.Print("Erreur de réception d'un message provenant de ", p.addr)
 		}
-
+		myClock.access.Lock()
+		myClock.clockMap[fmt.Sprint("127.0.0.1:", myClock.myPort)] += 1
+		myClock.access.Unlock()
 		// Les messages reçus sont de la forme type:::message où type
 		// vaut "private" pour les messages privés
+		//type:::clock:::message
+		//type clock message
+		//clock -> clock
+		//garde la plus ancienne
 		splitMsg := strings.Split(msg, ":::")
 
+		clockMap := map[string]int{}
+		for _, row := range strings.Split(splitMsg[1], ";") {
+			split := strings.Split(row, ",")
+			clockMap[split[0]], _ = strconv.Atoi(split[1])
+		}
+
 		receiveChan <- chatMessage{
-			from:    p.addr,
-			content: splitMsg[1],
-			private: splitMsg[0] == "private",
+			from:      p.addr,
+			content:   splitMsg[2],
+			fromClock: clockMap,
+			private:   splitMsg[0] == "private",
 		}
 	}
 
@@ -59,22 +74,34 @@ func (p peer) receiveMessages(receiveChan chan chatMessage) {
 // et écritures dans des canaux (chan) se font sans soucis liés aux
 // accès concurrents aux ressources, il n'est donc pas la peine
 // d'ajouter un verou
-func (p peer) sendMessages() {
+func (p peer) sendMessages(myClock *clock) {
 
 	writer := bufio.NewWriter(p.conn)
 
 	for {
 		msg := <-p.toSend
 
+		//On met l'horloge du pair acutel en string pour le protocole
+		protocoleClock := ""
+		myClock.access.RLock()
+		for index, value := range myClock.clockMap {
+			protocoleClock += index + "," + strconv.Itoa(value) + ";"
+		}
+		myClock.access.RUnlock()
+		//on retire le dernier ";"
+		protocoleClock = protocoleClock[:len(protocoleClock)-1]
 		protocolMsg := msg.content
+
+		//protocole : type:::clock:::message
 		if msg.private {
-			protocolMsg = "private:::" + protocolMsg
+			protocolMsg = "private:::" + protocoleClock + ":::" + protocolMsg
 		} else {
-			protocolMsg = "public:::" + protocolMsg
+			protocolMsg = "public:::" + protocoleClock + ":::" + protocolMsg
 		}
 
-		log.Print("J'envoie ceci : '", protocolMsg[:len(protocolMsg)-1], "' à ", p.addr)
-
+		timeSleeping := rand.Intn(10)
+		log.Print("J'envoie ceci : '", protocolMsg[:len(protocolMsg)-1], "' à ", p.addr, " avec un délai de ", timeSleeping, " secondes.")
+		time.Sleep(time.Second * time.Duration(timeSleeping))
 		_, err1 := writer.WriteString(protocolMsg)
 		err2 := writer.Flush()
 		if err1 != nil || err2 != nil {
@@ -85,7 +112,7 @@ func (p peer) sendMessages() {
 
 // Récupère les messages entrés au clavier par l'utilisateur et les
 // place dans les canneaux des bons pairs pour envoie futur
-func (peers *peerCollection) prepareChatMessages() {
+func (peers *peerCollection) prepareChatMessages(myClock *clock) {
 
 	reader := bufio.NewReader(os.Stdin)
 
@@ -104,7 +131,9 @@ func (peers *peerCollection) prepareChatMessages() {
 		// c'est ok car on ne va pas modifier cette structure
 		// mais seulement écrire dans un canal
 		peers.access.RLock()
-
+		myClock.access.Lock()
+		myClock.clockMap["127.0.0.1:"+myClock.myPort] += 1
+		myClock.access.Unlock()
 		if peerID, err := strconv.Atoi(splitInput[0]); len(splitInput) >= 2 && err == nil && peerID < len(peers.peers) {
 			peers.peers[peerID].toSend <- chatMessage{
 				content: strings.Join(splitInput[1:], ""),
@@ -125,19 +154,51 @@ func (peers *peerCollection) prepareChatMessages() {
 
 // Traite les messages du canal receiveChan pour les afficher sur
 // la sortie standard
-func displayChatMessages(receiveChan chan chatMessage) {
+func displayChatMessages(receiveChan chan chatMessage, myClock *clock) {
+	msg := chatMessage{}
 	for {
-		msg := <-receiveChan
-		if msg.private {
+		msgToCompare := chatMessage{}
+		if reflect.DeepEqual(msg, chatMessage{}) {
+			msg = <-receiveChan
+		}
+
+		msgToCompare = <-receiveChan
+
+		//compare
+		msg, msgToCompare = compareClockMap(msg, msgToCompare)
+
+		//update de myclock
+		for index, value := range msgToCompare.fromClock {
+			if myClock.clockMap[index] <= value {
+				myClock.clockMap[index] = value
+			}
+		}
+
+		//prendre en compte le message
+		if msgToCompare.private {
 			fmt.Print(
-				"Message privé de ", msg.from, ": ",
-				msg.content,
+				"Message privé de ", msgToCompare.from, ": ",
+				msgToCompare.content,
 			)
 		} else {
 			fmt.Print(
-				"Message de ", msg.from, ": ",
-				msg.content,
+				"Message de ", msgToCompare.from, ": ",
+				msgToCompare.content,
 			)
 		}
 	}
+}
+
+func compareClockMap(message chatMessage, messageToCompare chatMessage) (chatMessage, chatMessage) {
+	isBefore := false
+	for index, value := range message.fromClock {
+		valueToCompare := messageToCompare.fromClock[index]
+		if value <= valueToCompare {
+			isBefore = true
+		}
+	}
+	if isBefore {
+		return messageToCompare, message
+	}
+	return message, messageToCompare
 }
--- main.go
+++ main.go
@@ -1,12 +1,14 @@
 package main
 
 import (
-	"bufio"   // pour simplifier les lectures/écritures sur le réseau
-	"flag"    // pour simplifier la lecture de la ligne de commande
-	"fmt"     // pour construire des chaînes de caratères
-	"log"     // pour afficher des informations sur le déroulement du programme
-	"net"     // pour réaliser la connexion réseau
+	"bufio" // pour simplifier les lectures/écritures sur le réseau
+	"flag"  // pour simplifier la lecture de la ligne de commande
+	"fmt"   // pour construire des chaînes de caratères
+	"log"   // pour afficher des informations sur le déroulement du programme
+	"net"   // pour réaliser la connexion réseau
+	"strconv"
 	"strings" // pour manipuler des chaînes de caractères
+	"sync"
 )
 
 // Le type peer représente la connaissance qu'on a d'un pair du réseau, pour
@@ -18,8 +20,13 @@ type peer struct {
 	toSend chan chatMessage // AJOUT MINI-PROJET 3
 }
 
-func main() {
+type clock struct {
+	clockMap map[string]int
+	myPort   string
+	access   sync.RWMutex
+}
 
+func main() {
 	// Lecture des arguments de la ligne de commande : un port obligatoire, une
 	// adresse et un port facultatifs, un booléen first qui permet de préciser
 	// si on souhaite créer un nouveau réseau ou pas (et donc rejoindre un réseau)
@@ -51,6 +58,17 @@ func main() {
 	// afin de les afficher
 	receiveChan := make(chan chatMessage, 10)
 
+	//Horloge
+	myClock := clock{
+		clockMap: map[string]int{"127.0.0.1:" + strconv.Itoa(*port): 0},
+		myPort:   strconv.Itoa(*port),
+	}
+
+	//moi-même
+	myClock.access.RLock()
+	log.Print("CLOCK --- Initialisation : ", myClock.clockMap)
+	myClock.access.RUnlock()
+
 	// Si on n'est pas le premier, on rentre en contact avec notre point d'entrée
 	// sur le réseau pour lui demander les informations sur les autres pairs
 	if !(*isFirst) {
@@ -62,10 +80,12 @@ func main() {
 		if err != nil {
 			log.Fatal("Impossible de se connecter à l'adresse ", dialAddr)
 		}
-
+		myClock.access.Lock()
+		myClock.clockMap[dialAddr] = 0
+		myClock.access.Unlock()
 		// Demande des autres pairs
 		// MODIFICATION MINI-PROJET 3
-		otherPeers.peers = handleArrival(conn, *port)
+		otherPeers.peers = handleArrival(conn, *port, &myClock)
 
 		// Constitution du réseau en y ajoutant le point d'entrée
 		// MODIFICATION MINI-PROJET 3
@@ -79,20 +99,23 @@ func main() {
 		// MODIFICATION MINI-PROJET 3
 		displayNetwork(otherPeers.peers)
 	}
+	myClock.access.RLock()
+	log.Print("CLOCK --- Après avoir rejoint le réseau : ", myClock)
+	myClock.access.RUnlock()
 
 	// AJOUT MINI-PROJET 3
 	// Pour chaque pair du réseau on lance la routine qui permettra de
 	// lui envoyer des messages et la goroutine qui permettra de recevoir ses messages
 	for _, peer := range otherPeers.peers {
-		go peer.receiveMessages(receiveChan)
-		go peer.sendMessages()
+		go peer.receiveMessages(receiveChan, &myClock)
+		go peer.sendMessages(&myClock)
 	}
 
 	// AJOUT MINI-PROJET 3
 	// On démarre les routines qui vont permettre d'écrire des messages pour les
 	// autres pairs et d'afficher les messages reçus
-	go otherPeers.prepareChatMessages()
-	go displayChatMessages(receiveChan)
+	go otherPeers.prepareChatMessages(&myClock)
+	go displayChatMessages(receiveChan, &myClock)
 
 	// Après avoir pris connaissance des autres, on se met en écoute
 	listenPort := fmt.Sprint(":", *port)
@@ -113,7 +136,7 @@ func main() {
 			continue
 		}
 		log.Print("Un nouveau pair vient de me contacter")
-		ip, ok := handleConnection(conn, &otherPeers)
+		ip, ok := handleConnection(conn, &otherPeers, &myClock)
 		if ok {
 			// MODIFICATION MINI-PROJET 3
 			// réservation en écriture de la ressource avant de la modifier
@@ -137,8 +160,11 @@ func main() {
 			// Une fois le pair intégré dans le réseau, on lance la goroutine
 			// qui permettra de lui envoyer des messages et la goroutine qui
 			// permettra de recevoir ses messages
-			go newPeer.receiveMessages(receiveChan)
-			go newPeer.sendMessages()
+			go newPeer.receiveMessages(receiveChan, &myClock)
+			go newPeer.sendMessages(&myClock)
+			myClock.access.RLock()
+			log.Print("CLOCK --- Nouvelle utilisateur ayant rejoint, j'update mon horloge : ", myClock)
+			myClock.access.RUnlock()
 		}
 
 	}
@@ -147,7 +173,7 @@ func main() {
 
 // handleConnection gère une demande de connexion d'un nouveau pair
 // MODIFICATION MINI-PROJET 3 : otherPeers n'est plus seulement un tableau de peer
-func handleConnection(conn net.Conn, otherPeers *peerCollection) (string, bool) {
+func handleConnection(conn net.Conn, otherPeers *peerCollection, myClock *clock) (string, bool) {
 
 	reader := bufio.NewReader(conn)
 
@@ -209,13 +235,16 @@ func handleConnection(conn net.Conn, otherPeers *peerCollection) (string, bool) 
 	ip = ip[:splitPoint]
 	ip += ":" + splitMsg[1]
 	ip = strings.TrimSuffix(ip, "\n")
+	myClock.access.Lock()
+	myClock.clockMap[ip] = 0
+	myClock.access.Unlock()
 	return ip, true
 }
 
 // handleArrival gère notre arrivée dans le réseau : prise de contact avec
 // notre point d'entrée, récupération des adresses de tous les autres pairs
 // MODIFICATION MINI-PROJET 3 : un argument ajouté, receiveChan
-func handleArrival(conn net.Conn, myPort int) []peer {
+func handleArrival(conn net.Conn, myPort int, myClock *clock) []peer {
 
 	reader := bufio.NewReader(conn)
 	writer := bufio.NewWriter(conn)
@@ -258,6 +287,9 @@ loop:
 					toSend: make(chan chatMessage, 10),
 				} // AJOUT MINI-PROJET 3
 				peers = append(peers, newPeer)
+				myClock.access.Lock()
+				myClock.clockMap[fullAddr] = 0
+				myClock.access.Unlock()
 			}
 		case "done\n":
 			break loop
